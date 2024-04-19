371b58aaba95732b0877aa1687c4c3492b898d82
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-04-19 23:14:53 +0200

    web app backend created, POSTmethod tested with postman successfully, all descriptions added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Temp-Conversion-DevOps
+
+## Table of contents: 
+* [General info](#general-info)
+* [Steps and used technologies](#steps-and-used-technologies)
+* [Requirements](#requirements)
+* [Setup](#setup)
+
+### General info
+The aim of this project is to create a comprehensive environment for a web application based on DevOps principles.
+### Steps and used technologies
+1. Creation of backend server that uses HTTP protocol and response with calculated Celsius temperature based on Fahrenheit temperature -> Python 3.11+, Flask
+### Requirements
+### Setup
---
+++ app/run.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)  # Flask instance creation
+app_identifier = os.urandom(24).hex()  # creation of app identifier
+
+
+def covert_fahrenheit_to_celsius(fahrenheit_temp):
+    """
+    calculate Celsius temperature based on Fahrenheit temperature
+    :param fahrenheit_temp: Fahrenheit temperature
+    :return: Celsius temperature
+    """
+    return (fahrenheit_temp - 32) * (5 / 9)
+
+
+@app.route("/convert/", methods=["POST"])
+def covert_temperature():
+    """
+    converts Fahrenheit temperature to  Celsius temperature,
+    expects JSON formatted data with key: "fahrenheit" containing temperature value (int or float)
+    :return: JSON formatted data: if success: Celsius temperature and app identifier randomly
+    generated inside the app during startup, if key 'fahrenheit' is missing or temperature value
+    has incorrect type: error message
+    """
+    try:
+        data = request.get_json()
+        fahrenheit_temperature = data.get("fahrenheit")
+        if fahrenheit_temperature is None:
+            raise ValueError("Missing 'fahrenheit' value")
+        if not isinstance(fahrenheit_temperature, (float, int)):
+            raise TypeError("Fahrenheit temperature should be a number")
+        celsius_temperature = covert_fahrenheit_to_celsius(fahrenheit_temperature)
+        return jsonify({
+            "celsius": celsius_temperature,
+            "app_identifier": app_identifier
+        }), 200
+    except (ValueError, TypeError) as e:
+        return jsonify({'Error occurred': str(e)}), 400
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
